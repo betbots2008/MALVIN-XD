e12b304e1a01d4d319964b9c3b639ce1c1a1c3c6
betbots2008 <emmanueltbabawale@gmail.com>
2025-07-31 19:59:43 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~oiUHVabR#lX-g0iCSBLZkkVzXzoldWOI_3zPgrD7-arsEFuA64pQ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "2348149982012", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
